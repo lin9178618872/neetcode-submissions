268ebe67b1b56c00915298dc582435f046a1a530
lin9178618872 <62189686+lin9178618872@users.noreply.github.com>
2026-05-25 01:04:50 -0400

    Add: employee-free-time - submission-9

---
+++ Data Structures & Algorithms/employee-free-time/submission-9.py
@@ -0,0 +1,35 @@
+# Definition for an Interval.
+# class Interval:
+#     def __init__(self, start: int = None, end: int = None):
+#         self.start = start
+#         self.end = end
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        intervals = []
+
+        for employee in schedule:
+            for interval in employee:
+                intervals.append([interval.start, interval.end])
+#[x,y]，[x,y]靠看前面x来排
+        intervals.sort(key=lambda x: x[0])
+#合并区间，用0组跟1...n一个一个比较
+        merged = [intervals[0]]
+
+        for start, end in intervals[1:]:
+            last_end = merged[-1][1]#第0个数组的后面数默认为last_end
+
+            if start <= last_end:#[0,1],[3,4]->0<4对
+                merged[-1][1] = max(last_end, end)#换一个，1<4=4->变成[0,4]
+            else:
+                merged.append([start, end])#插入去并存
+#找出了空隙区间
+        free = []
+
+        for i in range(1, len(merged)):#这里的merged默认是有0的，但是要从1开始
+            prev_end = merged[i - 1][1]
+            curr_start = merged[i][0]
+
+            if prev_end < curr_start:
+                free.append(Interval(prev_end, curr_start))
+
+        return free
