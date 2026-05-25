fade4f2b7e4e9ff8699d92c8c15b461274eadd2b
lin9178618872 <62189686+lin9178618872@users.noreply.github.com>
2026-05-24 20:55:25 -0400

    Add: evaluate-division - submission-8

---
+++ Data Structures & Algorithms/evaluate-division/submission-8.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+
+class Solution:
+    def calcEquation(self, equations, values, queries):
+
+        graph = defaultdict(list)
+
+        for i in range(len(equations)):
+
+            a, b = equations[i]
+            val = values[i]
+
+            graph[a].append((b, val))
+            graph[b].append((a, 1 / val))
+
+        # 统一模板
+        def dfs(cur, visited, extra):
+            target=extra
+
+            # 终止条件
+            if cur == target:
+                return 1.0
+
+            # 标记
+            visited.add(cur)
+
+            # 遍历邻居
+            for nei, weight in graph[cur]:
+
+                if nei in visited:#a,c,a,c
+                    continue
+
+                sub = dfs(nei, visited, target)
+
+                if sub != -1:#跟前面=1
+                    return weight * sub
+
+            return -1
+
+        ans = []
+
+        for a, b in queries:
+
+            if a not in graph or b not in graph:
+                ans.append(-1.0)
+
+            else:
+                ans.append(dfs(a, set(), b))
+
+        return ans
